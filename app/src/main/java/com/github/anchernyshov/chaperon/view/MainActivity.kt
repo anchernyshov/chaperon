be9ff2559fbb442366cf497be23d69c207d7c19f
anchernyshov <81495546+anchernyshov@users.noreply.github.com>
2024-05-21 19:13:07 +0300

    Change default appication activity to MainActivity

--- app/src/main/java/com/github/anchernyshov/chaperon/view/MainActivity.kt
+++ app/src/main/java/com/github/anchernyshov/chaperon/view/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.github.anchernyshov.chaperon.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.github.anchernyshov.chaperon.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +19,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val intent = Intent(
+            this@MainActivity,
+            AuthActivity::class.java
+        )
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
     }
 }
