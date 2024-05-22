7cadb2a0b05b3f36d05eb6e9a4f42b5da0029be1
anchernyshov <81495546+anchernyshov@users.noreply.github.com>
2024-05-22 21:52:15 +0300

    Create ContentActivity class

---
+++ app/src/main/java/com/github/anchernyshov/chaperon/view/ContentActivity.kt
@@ -0,0 +1,21 @@
+package com.github.anchernyshov.chaperon.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.github.anchernyshov.chaperon.R
+
+class ContentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_content)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
